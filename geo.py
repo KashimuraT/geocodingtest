45044d39d668951fc0afe2b809ea0aa869178215
bp18030 <bp18030@shibaura-it.ac.jp>
2018-10-03 18:28:34 +0900

    <second commit>

--- geo.py
+++ geo.py
@@ -1,7 +1,7 @@
 import requests
-
+print("場所を入力してください")
 full_address = input()
- #東京都庁の住所である
+
 url = 'http://www.geocoding.jp/api/'
 payload = {'q': full_address}
 result = requests.get(url, params=payload)
@@ -9,5 +9,7 @@ result.text
 import xmltodict #ない場合はpip install xmltodictすると入手できます
 resultdict = xmltodict.parse(result.text)
 resultdict
+print("緯度")
 print(resultdict["result"]["coordinate"]["lat"])
+print("経度")
 print(resultdict["result"]["coordinate"]["lng"])
